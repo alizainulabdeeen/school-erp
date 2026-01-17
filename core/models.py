3317898fd3d53f414983260fa337a0d36b52bab3
Ali Zain <133251060+alizainulabdeeen@users.noreply.github.com>
2026-01-17 20:38:16 +0500

    feat(auth): add accounts app and user model (#1)
    * feat(auth): add accounts app and user model
    
    * resolved the problems that were mentioned on comments
    
    * solved all commented issues
    
    * solved typo for debug and removed unneccessary files
    
    ---------
    
    Co-authored-by: iamzaynawan <133251060+iamzaynawan@users.noreply.github.com>

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from model_utils.models import TimeStampedModel
+import uuid
+
+class BaseModel(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
